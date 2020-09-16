2cee5876ba7ffeba11a42c59bc1f49cf11de902d
Harsh Jaiswal <66635990+Harsh-Jaiswal2307@users.noreply.github.com>
2020-09-16 16:49:10 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-// #include <conio.h>
+
 
 char square[10] = {'o', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
 int choice, player;         // choice - what user entered player ( 1- 9)
